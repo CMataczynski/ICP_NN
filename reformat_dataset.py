3446f70f0abc57b4519e8e3e68ea686181bbdd59
MaczekO <c.matacz@gmail.com>
2020-02-20 20:13:54 +0100

    reformat_dataset_changed

--- reformat_dataset.py
+++ reformat_dataset.py
@@ -6,10 +6,10 @@ from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 # path to full, raw dataset
-dataset_name = "full_corrected_dataset"
-datasets = os.path.join(os.getcwd(), "datasets", "RAW_corrected_dataset")
+dataset_name = "full_splitted_dataset"
+datasets = os.path.join(os.getcwd(), "datasets", "RAW_splitted")
 new_path = os.path.join(os.getcwd(), "datasets", dataset_name)
-
+split = 0.25
 # traverse root directory, and list directories as dirs and files as files
 map_arr = []
 t1 = []
@@ -40,14 +40,16 @@ for root, dirs, files in tqdm(os.walk(datasets)):
 mapping = pd.DataFrame(data=map_arr, columns=["new_id", "RAW_path"])
 mapping.to_csv(os.path.join(os.getcwd(), "datasets", dataset_name + "_to_RAW_mapping.csv"), sep=';')
 
-t1_train, t1_test = train_test_split(t1, test_size=0.25)
-t2_train, t2_test = train_test_split(t2, test_size=0.25)
-t3_train, t3_test = train_test_split(t3, test_size=0.25)
-t4_train, t4_test = train_test_split(t4, test_size=0.25)
-ae_train, ae_test = train_test_split(ae, test_size=0.25)
+t1_train, t1_test = train_test_split(t1, test_size=split)
+t2_train, t2_test = train_test_split(t2, test_size=split)
+t3_train, t3_test = train_test_split(t3, test_size=split)
+t4_train, t4_test = train_test_split(t4, test_size=split)
+ae_train, ae_test = train_test_split(ae, test_size=split)
 
 X_train = t1_train + t2_train + t3_train + t4_train + ae_train
 X_test = t1_test + t2_test + t3_test + t4_test + ae_test
+
+# X_train = X_train + X_test
 
 if not os.path.exists(new_path):
     os.mkdir(new_path)
