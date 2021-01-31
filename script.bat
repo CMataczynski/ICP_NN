61e5629b895afa8eac640d7d560505e8c0e29102
MaczekO <c.matacz@gmail.com>
2021-01-31 18:47:13 +0100

    Code cleanup #3

--- script.bat
+++
@@ -1,3 +0,0 @@
-call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\Users\cmata\.conda\envs\icpenv
-python multiexp.py
-PAUSE
--- tb.bat
+++
@@ -1,3 +0,0 @@
-call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\Users\cmata\.conda\envs\icpenv
-tensorboard --logdir experiments
-PAUSE
